9c6292a3ff45d7423b2f45878b2a1ec89afb63ae
nakshathru <nakshathru@gmail.com>
2019-02-22 23:39:17 +0530

    new server with babel

--- build/index.js
+++ build/index.js
@@ -6,8 +6,11 @@ function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { de
 
 var app = (0, _express.default)();
 app.get('/', function (req, res) {
-  return res.send('Hello World!');
+  res.status(200).json({
+    ' message': 'Welcome to Node.js & Express'
+  });
 });
-app.listen(4000, function () {
-  return console.log("Express server running on port 4000");
+app.listen(process.env.PORT || 3000, function () {
+  return console.log("Listening to port 3000");
 });
+//# sourceMappingURL=index.js.map
